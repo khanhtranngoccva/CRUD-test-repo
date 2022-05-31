dc9dd0f08bc02e0d5d0616eafb723e36914003c2
khanhtranngoccva <khanhtranngoccva@gmail.com>
2022-05-31 13:59:47 +0700

    Refactoring

---
+++ config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require("express");
+const mongodb = require("mongodb");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 5000;
+const expressApp = express();
+
+const dbUsername = process.env.MONGODB_USER_NAME;
+const dbPassword = process.env.MONGODB_USER_PASSWORD;
+
+const mongoURL = `mongodb+srv://${dbUsername}:${dbPassword}@starwarscrud.txzc8qa.mongodb.net/?retryWrites=true&w=majority`;
+const mongoClient = new mongodb.MongoClient(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: mongodb.ServerApiVersion.v1,
+});
+
+expressApp.set("view engine", "ejs");
+expressApp.use("/static", express.static(path.join(__dirname, "public")));
+expressApp.use(bodyParser.urlencoded({extended: true}));
+expressApp.listen(PORT, function() {
+    console.log(`Listening on port ${PORT}.`);
+});
+
+module.exports = {expressApp, mongoClient};
--- server.js
+++ server.js
@@ -1,33 +1,14 @@
-const express = require("express");
-const mongodb = require("mongodb");
 const path = require("path");
-const bodyParser = require("body-parser");
+const {mongoClient, expressApp} = require("./config");
 
-require('dotenv').config();
-
-const PORT = process.env.PORT || 5000;
-const expressApp = express();
-
-const dbUsername = process.env.MONGODB_USER_NAME;
-const dbPassword = process.env.MONGODB_USER_PASSWORD;
-
-const mongoURL = `mongodb+srv://${dbUsername}:${dbPassword}@starwarscrud.txzc8qa.mongodb.net/?retryWrites=true&w=majority`;
-const mongoClient = new mongodb.MongoClient(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: mongodb.ServerApiVersion.v1});
-
-expressApp.set("view engine", "ejs");
-expressApp.use("/static", express.static(path.join(__dirname, "public")));
-expressApp.use(bodyParser.urlencoded({extended: true}));
 
 async function main() {
+    console.log("Connecting!");
     await mongoClient.connect();
     console.log("Connected to MongoDB database.");
 
     const quotesDB = mongoClient.db("starWarsQuotes");
     const quotesCollection = quotesDB.collection("starWarsQuotesCollection");
-
-    expressApp.listen(PORT, function() {
-        console.log(`Listening on port ${PORT}.`);
-    });
 
     expressApp.get("/", async (req, res) => {
         const cursor = quotesCollection.find();
